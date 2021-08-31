3971b3f47ddf69b03e23297ebed335c0b4742b14
andriwskenda <skenda.computers@gmail.com>
2021-08-31 17:29:09 -0500

    first commit

---
+++ Prueba2/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Skenda_Prueba2', __DIR__);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Skenda
+# Skenda
